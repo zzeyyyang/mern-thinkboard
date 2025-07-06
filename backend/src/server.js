375809863b93cee8e9c02befbba5d3a879762624
Zeyang <zeyyyang@gmail.com>
2025-07-06 09:10:38 -0700

    fix frontend path

--- backend/src/server.js
+++ backend/src/server.js
@@ -28,7 +28,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
   app.use("*", (_, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../../frontend", "dist", "index.html"));
   });
 }
 
